643d610fd109bcb29652ceffee38f41788fb2d33
nd <nd1012@users.noreply.github.com>
2024-04-28 17:36:45 +0200

    Update

---
+++ src/wan24-RPC/IRpcResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace wan24.RPC
+{
+    /// <summary>
+    /// Interface for a RPC response
+    /// </summary>
+    public interface IRpcResponse
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        long Id { get; }
+        /// <summary>
+        /// Return value
+        /// </summary>
+        object? ReturnValue { get; }
+    }
+}
